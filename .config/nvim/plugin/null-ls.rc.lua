056da76e9e567926b2091834e7e9881fb5faae54
2000923 <2000923@unmsm.edu.pe>
2023-11-18 03:51:18 -0500

    add new lines for check the syntaxis python

--- .config/nvim/plugin/null-ls.rc.lua
+++ .config/nvim/plugin/null-ls.rc.lua
@@ -18,7 +18,9 @@ null_ls.setup {
     null_ls.builtins.diagnostics.eslint_d.with({
       diagnostics_format = '[eslint] #{m}\n(#{c})'
     }),
-    null_ls.builtins.diagnostics.fish
+    null_ls.builtins.diagnostics.fish,
+    null_ls.builtins.diagnostics.mypy,
+    null_ls.builtins.diagnostics.ruff
   },
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
